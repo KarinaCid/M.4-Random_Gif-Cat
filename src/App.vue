4261ea3adefccfa1bcd3a58ab128ee7dcb92152b
Karina Cid <memento_mori.06@hotmail.com>
2020-10-06 20:10:14 -0300

    first commit proyect

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="" src="">
+    <RandomCat msg="Random Gif Cat"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RandomCat from './components/RandomCat'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RandomCat
   }
 }
 </script>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: lightblue;
 }
 </style>
---
+++ src/components/RandomCat.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <form>
+      <div class="form__input-container">
+        <div class="row form-group">
+          <div class="col-6 d-flex flex-column align-items-end">
+            <label for="text">Titulo</label>
+            <label for="text">Filtro</label>
+            <label for="text">Color</label>
+            <label for="text">Tamaño</label>
+          </div>
+          <div class="col-6 d-flex flex-column align-items-start">
+            <input type="text" />
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <input type="text" name="" id="" />
+          </div>
+        </div>
+      </div>
+
+      <input type="submit" value="obtener mi gato" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Random Gif Cat",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  font-weight: 700;
+  text-align: center;
+  padding: 2em 0 1em;
+}
+.form__input-container {
+  background-color: lightcoral;
+}
+.form__input-group {
+  margin: 1em;
+  text-align: left;
+}
+</style>
